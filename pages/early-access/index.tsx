f091abed5dfbc90008a02d9fb43942e9acfae4d4
Jeremy Carrera <jeremycarrera@MacBookPro.lan>
2026-02-16 16:30:46 -0700

    Add Meta Pixel to early access landing page

--- pages/early-access/index.tsx
+++ pages/early-access/index.tsx
@@ -11,6 +11,31 @@ export default function EarlyAccessPage() {
         <title>Early Access - ReviewFlo</title>
         <meta name="description" content="Get 2 months of full ReviewFlo access for just $10. Limited to 50 businesses." />
         <meta name="robots" content="noindex, nofollow" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '2269529710245007');
+      fbq('track', 'PageView');
+    `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=2269529710245007&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </Head>
 
       <div className="min-h-screen bg-gradient-to-br from-[#F5F5DC] via-white to-[#F5F5DC]">
