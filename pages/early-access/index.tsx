6659f78ce94755dd7efa1eeaeadbe6cff952b12c
Jeremy Carrera <jeremycarrera@MacBookPro.lan>
2026-02-16 17:21:24 -0700

    Use Meta Pixel ID 1269800455347496

--- pages/early-access/index.tsx
+++ pages/early-access/index.tsx
@@ -22,7 +22,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '2269529710245007');
+fbq('init', '1269800455347496');
 fbq('track', 'PageView');`,
           }}
         />
@@ -31,7 +31,7 @@ fbq('track', 'PageView');`,
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=2269529710245007&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1269800455347496&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
